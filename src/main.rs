0fd373700b6119f8e72803249917e56fbf514b32
rexx07 <56883674+rexx07@users.noreply.github.com>
2022-11-26 22:14:35 -0500

    connect db

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use newsletter_app::run;
+use newsletter_app::startup::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+        let listener: TcpListener = TcpListener::bind("127.0.0.1:0")
+            .expect("Failed to bind random port");
+        run(listener)?.await
 }
